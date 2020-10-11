4ca8847ff53a1e53b951e3968507b5ed15a53eaf
Dave Roverts <me@daveroverts.nl>
2020-10-11 13:56:08 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const { prefix, token, tables } = require('./config.json');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+  // eslint-disable-next-line no-console
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(token);
+
+client.on('message', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const command = message.content.trim().toLowerCase();
+
+  switch (command) {
+    case 'mand':
+      message.channel.send('MAND!');
+      break;
+    case 'flip':
+      message.channel.send(tables[Math.floor(Math.random() * tables.length)]);
+      break;
+    case 'unflip':
+      message.channel.send('┬─┬ ノ( ゜-゜ノ)');
+      break;
+    default:
+  }
+});
